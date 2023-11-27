b51e1d5a2442b6cdb40688054a7a7ca488252d83
volenterk <volenter.krisztian0525@gmail.com>
2023-11-27 03:17:35 +0100

    Adding changes to cypress testing, although the tests dont work anymore (cause: 1d7508835ff22cb753940710ea687b93d023774e)

--- cypress/e2e/user/user-create.cy.ts
+++ cypress/e2e/user/user-create.cy.ts
@@ -4,15 +4,24 @@ beforeEach(() => {
 })
 
 describe('Create users', () => {
-    it('Should create test@te.st employee', () => {
+    it('Should return 200 response code on creating test@te.st employee', () => {
         const randomSuffix = new Date().getTime();
         cy.createUser('test' + randomSuffix + '@te.st')
 
         cy.wait('@createUser').then((interception) => {
             expect(interception.response.statusCode).to.equal(200);
         });
+        it('Should be visible after creation', () => {
+            //TODO cant do it because the users list does not have any text in it
+            /*const randomSuffix = new Date().getTime();
+            cy.createUser('test' + randomSuffix + '@te.st')
+    
+            cy.wait('@createUser').then((interception) => {
+                expect(interception.response.statusCode).to.equal(200);
+            });*/
+        })
     })
-    it('Should not create test@te.st employee because it already exists', () => {
+    it('Should return 500 response code on creating test@te.st employee because it already exists', () => {
         cy.createUser('test@te.st')
         cy.createUser('test@te.st')
         cy.wait('@createUser').then((interception) => {
